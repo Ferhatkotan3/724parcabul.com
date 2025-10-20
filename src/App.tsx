0409245bb1b756b65f2df60f8aa502b56784edd4
Ferhatkotan3 <kotan.frht@gmail.com>
2025-10-20 14:49:56 +0300

    Force HashRouter only; rebuild docs for GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 
-import { useEffect } from 'react'
-import { HashRouter, BrowserRouter, useRoutes, useNavigate } from 'react-router-dom'
+import { HashRouter, useRoutes } from 'react-router-dom'
 import routes from './router/config'
 
 function AppRoutes() {
   return useRoutes(routes);
 }
 
-// GH Pages'te hash olmadan gelen kullanıcıyı #/ rotasına yönlendirir
-function HashRedirectGuard() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const hasHash = window.location.hash && window.location.hash.length > 1;
-    if (!hasHash) {
-      navigate('/', { replace: true });
-    }
-  }, [navigate]);
-
-  return <AppRoutes />;
-}
-
 function App() {
-  const isProd = import.meta.env.PROD
-  return isProd ? (
+  return (
     <HashRouter basename="/">
-      <HashRedirectGuard />
-    </HashRouter>
-  ) : (
-    <BrowserRouter basename={typeof __BASE_PATH__ !== 'undefined' ? __BASE_PATH__ : '/'}>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
